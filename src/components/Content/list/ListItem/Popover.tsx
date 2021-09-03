8f052babe7d19d58401615a068a1266ba23d15ba
jiayu <350600880@qq.com>
2021-09-03 16:34:23 +0800

    [jiayu] fix bug when add resource click outside cannot close the window

--- src/components/Content/list/ListItem/Popover.tsx
+++ src/components/Content/list/ListItem/Popover.tsx
@@ -6,74 +6,78 @@ import {NewAgentType} from "../../../../type";
 import {updateResourceAction} from "../../../../server/server";
 
 interface PopoverProps {
-    IfPopoverWindowOpen: (id: number, status: boolean) => void
-    agentShow: NewAgentType
-    addResources: (id: number, resources: Array<string>) => void
-    id: number
+  IfPopoverWindowOpen: (id: number, status: boolean) => void
+  agentShow: NewAgentType
+  addResources: (id: number, resources: Array<string>) => void
+  id: number
 }
 
 const Popover = ({IfPopoverWindowOpen, agentShow, addResources, id}: PopoverProps) => {
-    const [newResources, setNewResources] = useState('')
-    const [popoverInput, setPopoverInput] = useState('')
+  const [newResources, setNewResources] = useState('')
+  const [popoverInput, setPopoverInput] = useState('')
 
-    const inputRef = React.createRef<HTMLDivElement>();
-    useEffect(() => {
-        document.addEventListener('click', handleClickOutsidePopover)
-        return () => {
-            document.removeEventListener('click', handleClickOutsidePopover)
-        }
-    }, [])
-
-
-    const handlePopoverInput = (e: any) => {
-        setPopoverInput(e.target.value)
-        setNewResources(e.target.value)
+  const inputRef = React.createRef<HTMLDivElement>();
+  useEffect(() => {
+    document.addEventListener('click', handleClickOutsidePopover)
+    return () => {
+      document.removeEventListener('click', handleClickOutsidePopover)
     }
-    const handleCloseWindow = (id: number) => {
-        IfPopoverWindowOpen(id, false)
-    }
-
-    const handleAddResource = async (id: number, newResources: string) => {
-        const resourcesAdd = newResources.split(',')
-        await updateResourceAction(id, [...agentShow.resources, ...(resourcesAdd)])
-        addResources(id, resourcesAdd)
-        setPopoverInput('')
-    }
+  }, [popoverInput])
 
 
-    const handleClickOutsidePopover = (e: Event) => {
-        if (!inputRef.current) {
-            return
-        }
-        if (!inputRef.current.contains(e.target as Node) && inputRef.current !== e.target) {
-            IfPopoverWindowOpen(id, false)
-        }
+  const handlePopoverInput = (e: any) => {
+    setPopoverInput(e.target.value)
+    setNewResources(e.target.value)
+  }
+  const handleCloseWindow = (id: number) => {
+    IfPopoverWindowOpen(id, false)
+  }
+
+  const handleAddResource = async (id: number, newResources: string) => {
+    const resourcesAdd = newResources.split(',')
+    await updateResourceAction(id, [...agentShow.resources, ...(resourcesAdd)])
+    addResources(id, resourcesAdd)
+    IfPopoverWindowOpen(id, false)
+    setPopoverInput('')
+  }
+
+
+  const handleClickOutsidePopover = (e: Event) => {
+    if (!inputRef.current) {
+      return
     }
 
-    return (
-        <section className='popover-section' ref={inputRef}>
-            <div className="popover">
-                <div className="popover_content">
-                    <div className="triangle"/>
-                    <p>Separte multiple resource name with commas</p>
-                    <input type="text" name="" placeholder={`e.g.Chrome,Firefox`}
-                           value={popoverInput}
-                           onChange={(e) => handlePopoverInput(e)}/>
-                    <div className="btns">
-                        <button type="button" className="add"
-                                onClick={() => handleAddResource(agentShow.id, newResources)}>Add Resources
-                        </button>
-                        <button type="button" className="cancel" name="button"
-                                onClick={() => handleCloseWindow(agentShow.id)}>Cancel
-                        </button>
-                    </div>
-                    <a
-                        href='..'
-                        className="close_btn icofont-close"
-                        onClick={() => handleCloseWindow(agentShow.id)}/>
-                </div>
-            </div>
-        </section>
-    )
+    if (!inputRef.current.contains(e.target as Node) && inputRef.current !== e.target) {
+      IfPopoverWindowOpen(id, false)
+    }
+  }
+
+  return (
+    <section className='popover-section' ref={inputRef}>
+      <div className="popover">
+        <div className="popover_content">
+          <div className="triangle"/>
+          <p>Separte multiple resource name with commas</p>
+          <input type="text" name="" placeholder={`e.g.Chrome,Firefox`}
+                 value={popoverInput}
+                 onChange={(e) => handlePopoverInput(e)}
+
+          />
+          <div className="btns">
+            <button type="button" className="add"
+                    onClick={() => handleAddResource(agentShow.id, newResources)}>Add Resources
+            </button>
+            <button type="button" className="cancel" name="button"
+                    onClick={() => handleCloseWindow(agentShow.id)}>Cancel
+            </button>
+          </div>
+          <a
+            href='..'
+            className="close_btn icofont-close"
+            onClick={() => handleCloseWindow(agentShow.id)}/>
+        </div>
+      </div>
+    </section>
+  )
 }
 export default Popover
